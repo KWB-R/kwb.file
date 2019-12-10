59c631727ce8f485bb20cdeea91ee47cf5c598b6
hsonne <hauke.sonnenberg@gmx.de>
2019-12-11 00:44:03 +0100

    Integrate different methods in to_subdir_matrix()
    and delete to_subdir_matrix_2()

--- R/to_subdir_matrix.R
+++ R/to_subdir_matrix.R
@@ -7,7 +7,7 @@
 #' @param result_type one of \code{c("matrix", "data.frame")}, specifying the
 #'   type of object to be returned
 #' @param dbg if \code{TRUE} debug messages are shown
-#'
+#' @param method integer specifying the implementation method. One of 1, 2, 3.
 #' @return matrix or data frame, depending on \code{result_type}
 #' 
 #' @importFrom kwb.utils asNoFactorDataFrame enlargeVector
@@ -24,7 +24,7 @@
 #' folder_matrix[folder_matrix[, 1] == "a1", ]
 #'
 to_subdir_matrix <- function(
-  paths, fill.value = "", result_type = "matrix", dbg = FALSE
+  paths, fill.value = "", result_type = "matrix", dbg = FALSE, method = 3
 )
 {
   stopifnot(result_type %in% c("matrix", "data.frame"))
@@ -42,14 +42,53 @@ to_subdir_matrix <- function(
     )
   }
 
-  # Get the maximum path depth
-  max_depth <- get_max_path_depth(paths)
+  stopifnot(method %in% 1:3)
+  
+  if (method == 1) {
 
-  # Extend all list entries to a length of <max_depth>, filling with <fill.with>
-  paths <- lapply(paths, kwb.utils::enlargeVector, max_depth, fill.value)
-
-  # Create a matrix of subdirectories
-  result <- matrix(unlist(paths), nrow = length(paths), byrow = TRUE)
+    # Get the maximum path depth
+    max_depth <- get_max_path_depth(paths)
+    
+    # Extend all list entries to a length of <max_depth>, filling with <fill.with>
+    paths <- lapply(paths, kwb.utils::enlargeVector, max_depth, fill.value)
+    
+    # Create a matrix of subdirectories
+    result <- matrix(unlist(paths), nrow = length(paths), byrow = TRUE)
+    
+  } else {
+    
+    path_depths <- lengths(paths)
+    
+    max_depth <- max(path_depths)
+    
+    if (method == 2) {
+      
+      subdirs <- do.call(rbind, lapply(split(paths, path_depths), function(x) {
+        depth <- length(x[[1]])
+        m <- matrix(unlist(x), byrow = TRUE, ncol = depth)
+        n_fill <- (max_depth - depth) * length(x)
+        matrix(c(m, rep_len(fill.value, n_fill)), ncol = max_depth)
+      }))
+      
+      row_order <- order(unlist(split(seq_along(paths), path_depths)))
+      
+    } else if (method == 3) {
+      
+      index_list <- split(seq_along(paths), path_depths)
+      
+      subdirs <- do.call(rbind, lapply(index_list, function(indices) {
+        depth <- path_depths[indices[1]]
+        cbind(
+          matrix(unlist(paths[indices]), byrow = TRUE, ncol = depth), 
+          matrix(fill.value, ncol = (max_depth - depth), nrow = length(indices))
+        )
+      }))
+      
+      row_order <- order(unlist(index_list))
+    }
+    
+    result <- subdirs[row_order, ]
+  }
 
   # Return the result as an object of the requested type
   if (result_type == "matrix") {
